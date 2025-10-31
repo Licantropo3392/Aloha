fd52ee02cd555ed9075e33d7da38875c0d14232e
Licantropo3392 <denisalbu287@gmail.com>
2025-10-31 17:13:47 +0100

    Refactor LuaDraw.cpp: streamline platform-specific code

--- src/LuaDraw.cpp
+++ src/LuaDraw.cpp
@@ -3,26 +3,13 @@
 #ifdef __3DS__
 #include <3ds.h>
 #include <citro2d.h>
+#else
+#include <raylib.h>
+#endif
 
-int LuaInit(lua_State *L)
-{
-    return 0;
-}
+extern int targetFPS;
 
-int LuaSetFPS(lua_State *L)
-{
-    return 0;
-}
-
-int LuaClose(lua_State *L)
-{
-    C2D_Fini();
-    C3D_Fini();
-
-    gfxExit();
-    return 0;
-}
-
+#ifdef __3DS__
 void DeltaTime(u64 *oldTime, double *deltaTime)
 {
     u64 currentTime = osGetTime();
@@ -30,110 +17,42 @@ void DeltaTime(u64 *oldTime, double *deltaTime)
     *deltaTime = (double)((currentTime - *oldTime) / 1000.0);
     *oldTime = currentTime;
 }
-
-int LuaRectangle(lua_State *L)
-{
-    float x = luaL_checknumber(L, 1);
-    float y = luaL_checknumber(L, 2);
-    float width = luaL_checknumber(L, 3);
-    float height = luaL_checknumber(L, 4);
-
-    int R = 255;
-    int G = 255;
-    int B = 255;
-    int A = 255;
-
-    if (lua_istable(L, 5))
-    {
-        lua_getfield(L, 5, "r");
-        R = lua_tointeger(L, -1);
-        lua_pop(L, 1);
-
-        lua_getfield(L, 5, "g");
-        G = lua_tointeger(L, -1);
-        lua_pop(L, 1);
-
-        lua_getfield(L, 5, "b");
-        B = lua_tointeger(L, -1);
-        lua_pop(L, 1);
-
-        lua_getfield(L, 5, "a");
-        A = lua_tointeger(L, -1);
-        lua_pop(L, 1);
-    }
-
-    u32 color = C2D_Color32(R, G, B, A);
-
-    C2D_DrawRectangle(x, y, 0, width, height, color, color, color, color);
-    return 0;
-}
-
-int LuaCircle(lua_State *L)
-{
-    float x = luaL_checknumber(L, 1);
-    float y = luaL_checknumber(L, 2);
-    float radius = luaL_checknumber(L, 3);
-
-    if (x <= 0 || y <= 0 || radius <= 0)
-        return 0;
-
-    int R = 255;
-    int G = 255;
-    int B = 255;
-    int A = 255;
-
-    if (lua_istable(L, 4))
-    {
-        lua_getfield(L, 4, "r");
-        R = lua_tointeger(L, -1);
-        lua_pop(L, 1);
-
-        lua_getfield(L, 4, "g");
-        G = lua_tointeger(L, -1);
-        lua_pop(L, 1);
-
-        lua_getfield(L, 4, "b");
-        B = lua_tointeger(L, -1);
-        lua_pop(L, 1);
-
-        lua_getfield(L, 4, "a");
-        A = lua_tointeger(L, -1);
-        lua_pop(L, 1);
-    }
-
-    u32 color = C2D_Color32(R, G, B, A);
-
-    C2D_DrawCircle(x, y, 0, radius, color, color, color, color);
-    return 0;
-}
-
-#else
-#include <raylib.h>
-
-extern int targetFPS;
+#endif
 
 int LuaInit(lua_State *L)
 {
+    #ifndef __3DS__
     int screenWidth = luaL_checkinteger(L, 1);
     int screenHeight = luaL_checkinteger(L, 2);
     const char *title = luaL_checkstring(L, 3);
 
     // SetConfigFlags(FLAG_VSYNC_HINT);
     InitWindow(screenWidth, screenHeight, title);
+    #endif
 
     return 0;
 }
 
 int LuaSetFPS(lua_State *L)
 {
+    #ifndef __3DS__
     targetFPS = luaL_checkinteger(L, 1);
     SetTargetFPS(targetFPS);
+    #endif
+
     return 0;
 }
 
 int LuaClose(lua_State *L)
 {
+    #ifdef __3DS__
+    C2D_Fini();
+    C3D_Fini();
+    gfxExit();
+    #else
     CloseWindow();
+    #endif
+
     return 0;
 }
 
@@ -144,10 +63,10 @@ int LuaRectangle(lua_State *L)
     float width = luaL_checknumber(L, 3);
     float height = luaL_checknumber(L, 4);
 
-    int R = WHITE.r;
-    int G = WHITE.g;
-    int B = WHITE.b;
-    int A = WHITE.a;
+    int R = 255;
+    int G = 255;
+    int B = 255;
+    int A = 255;
 
     if (lua_istable(L, 5))
     {
@@ -168,9 +87,14 @@ int LuaRectangle(lua_State *L)
         lua_pop(L, 1);
     }
 
+    #ifdef __3DS__
+    u32 color = C2D_Color32(R, G, B, A);
+    C2D_DrawRectangle(x, y, 0, width, height, color, color, color, color);
+    #else
     Color color = {(unsigned char)R, (unsigned char)G, (unsigned char)B, (unsigned char)A};
-
     DrawRectangle(x, y, width, height, color);
+    #endif
+
     return 0;
 }
 
@@ -180,10 +104,10 @@ int LuaCircle(lua_State *L)
     float y = luaL_checknumber(L, 2);
     float radius = luaL_checknumber(L, 3);
 
-    int R = WHITE.r;
-    int G = WHITE.g;
-    int B = WHITE.b;
-    int A = WHITE.a;
+    int R = 255;
+    int G = 255;
+    int B = 255;
+    int A = 255;
 
     if (lua_istable(L, 4))
     {
@@ -204,10 +128,13 @@ int LuaCircle(lua_State *L)
         lua_pop(L, 1);
     }
 
+    #ifdef __3DS__
+    u32 color = C2D_Color32(R, G, B, A);
+    C2D_DrawCircle(x, y, 0, radius, color, color, color, color);
+    #else
     Color color = {(unsigned char)R, (unsigned char)G, (unsigned char)B, (unsigned char)A};
-
     DrawCircle(x, y, radius, color);
+    #endif
+
     return 0;
 }
-
-#endif
